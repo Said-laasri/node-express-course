1230f119052ddd0c9733b142d1b7bd04183a4123
Said-laasri <saidlaasri3@gmail.com>
2023-04-24 08:56:01 +0100

    Numeric filter expression

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,12 +1,15 @@
 const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({}).select("name price");
+  const products = await Product.find({ price: { $gt: 30 } })
+    .sort("name")
+    .select("name price");
+
   res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort, fields } = req.query;
+  const { featured, company, name, sort, fields, numericFelters } = req.query;
   const queryObject = {};
 
   if (featured) {
@@ -20,8 +23,30 @@ const getAllProducts = async (req, res) => {
     queryObject.name = { $regex: name, $options: "i" };
   }
 
-  // console.log(queryObject);
-  let result = await Product.find(queryObject);
+  if (numericFelters) {
+    const opertatorMap = {
+      ">": "$gt",
+      ">=": "$gte",
+      "=": "$eq",
+      "<": "$lt",
+      "<=": "$lte",
+    };
+    const regEx = /\b(<|>|>=|=|<|<=)\b/g;
+    let filters = numericFelters.replace(
+      regEx,
+      (match) => `-${opertatorMap[match]}-`
+    );
+    const options = ["price", "rating"];
+    filters = filters.split(",").forEach((item) => {
+      const [field, operator, value] = item.split("-");
+      if (options.includes(field)) {
+        queryObject[field] = { [operator]: Number(value) };
+      }
+    });
+  }
+
+  console.log(queryObject);
+  let result = Product.find(queryObject);
   if (sort) {
     const sortList = sort.split(",").join(" ");
     result = result.sort(sortList);
@@ -35,9 +60,14 @@ const getAllProducts = async (req, res) => {
     const fieldsList = fields.split(",").join(" ");
     result = result.select(fieldsList);
   }
-  
-  const products = await result;
 
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+
+  const products = await result;
   res.status(200).json({ products, nbHits: products.length });
 };
 
